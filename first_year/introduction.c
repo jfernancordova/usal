5f4c74d10cb89085f4b499183981e65249cea9e4
josefernando.c <josefernando.c@geopagos.com>
2019-05-21 20:23:15 -0300

    WIP

---
+++ first_year/introduction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    printf("\n");
+
+    printf("10 > 5 is : %i\n", 10 > 5);
+    printf("\n");
+
+    printf("Result is: %i", 5 > 2);
+}
