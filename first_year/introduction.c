86a920bf8fcbfc0f14e28174f6956b04602b21d5
josefernando.c <josefernando.c@geopagos.com>
2019-05-21 21:06:32 -0300

    WIP

--- first_year/introduction.c
+++ first_year/introduction.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
+    int num;
+
     printf("Hello, World!");
     printf("\n");
 
@@ -8,4 +10,17 @@ int main() {
     printf("\n");
 
     printf("Result is: %i", 5 > 2);
+    printf("\n");
+
+    printf("Please, enter a number: ");
+    scanf("%i", &num);
+    printf("Your number is: %i\n", num);
+
+    char letter;
+
+    printf("Please, enter a letter: ");
+    letter = getchar();
+    printf("\n You have introduced the letter: %c", letter);
+
+    return 0;
 }
