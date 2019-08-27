55fe10cbe9e9dadcf915d7442ea54f497b380f91
José Fernando Cordova <jfernancordova@gmail.com>
2019-08-27 20:01:17 -0300

    Pointers and Array

---
+++ Programming/Pointers_Arrays/one.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char string[10];
+
+    printf("Insert the string: ");
+    scanf("%s", &string[0]);
+    printf("You Inserted: %s", string);
+
+    return 0;
+}
---
+++ Programming/README.md
@@ -0,0 +1,6 @@
+Overview USAL (Universidad del Salvador)
+
+1. Functions returns a value
+2. Process returns nothing
+3. Parameters Modifications (Swapping)
+4.
