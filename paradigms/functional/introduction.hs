c1bd1d35f8826da6ad45cdec1a7d33e76e837326
José Fernando Cordova <jfernancordova@gmail.com>
2019-09-16 23:25:37 -0300

    WIP

--- paradigms/functional/introduction.hs
+++ paradigms/functional/introduction.hs
@@ -1,28 +1,27 @@
--- Plus 
 plus a b = a + b
 
--- Plus Decimal
-plusDEC::Double->Double->Double
-plusDEC a b = a + b
+plusDec::Double->Double->Double
+plusDec a b = a + b
 
--- Find the min between two numbers
 min::Int->Int->Int
-min a b = if a < b then a else b
+min a b = if a > b then b else a
 
--- Find the major between two numbers
-major::Int->Int->Int
-major a b = if a > b then a else b
+max::Int->Int->Int
+max a b = if a > b then a else b
 
--- Conditional
 equals::Int->Int->Bool
 equals a b = if a == b then True else False
 
-major100::(Int, Int, Int)->Int
-major100 (a,b,c) = if a > 100 then b else c
+greatherThan100::(Int, Int, Int)->Int
+greatherThan100 (a, b, c) = if a > 100 then b else c
 
-greaterThan::(Int, Int, Int)->Int->Int
-greaterThan (a,b,c) n = if a > n then b else c 
+greatherThan::(Int, Int, Int)->Int
+greatherThan (a, b, c) n = if a > n then b else c
 
-prod::(Int, Int, Int)->Int->(Int,Int,Int)
-prod (a,b,c)n = (a * n, b * n, c * n)
+prod::(Int, Int, Int)->Int->(Int, Int, Int)
+prod (a, b, c) n = (a * n, b * n, c * n)
+
+service::(String, Double, String)->Double->(String, Double, Double)
+service (patent, kms, brand) kmsservice = if kms >= kmsservice then (patent, brand, kmsservice) else ("Don't need a service", 0,0)
+
 
