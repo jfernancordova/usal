01f0645d9d3ec5692500eabd8d011dac66d45efe
José Fernando Cordova <josefernando.c@geopagos.com>
2019-10-11 15:52:01 -0300

    WIP

--- paradigms/functional/assignments/second/one.hs
+++ paradigms/functional/assignments/second/one.hs
@@ -1,7 +1,21 @@
 -- runhaskell one.hs
 
-potency :: Int -> [Int]
-potency n = takeWhile (<n) (map (2^) [0..])
+negative:: Int -> String
+negative n = if n > 0 then "Error, the number is positive" else "Negative"
+
+between:: Int -> String
+between n = if n > -50 && n < 50 then "It is in range between -50 and 50" else "Error, the number is not in range between -50 and 50"
+
+major:: Int -> String
+major n = if n > 100 then "The number is greater than 100" else "Error, the number is not greater than 100"
 
 main = do 
-    print $ potency 20
+
+    putStrLn "Enter value:"
+    input <- getLine
+
+    let value = (read input :: Int)
+
+    print $ negative value
+    print $ between value
+    print $ major value
--- paradigms/functional/classes/ten.hs
+++ paradigms/functional/classes/ten.hs
@@ -2,7 +2,7 @@
 
 student :: [(String, Int)] -> [(String, Int)]
 student [] = []
-student (stu, note) : xs  = if note > 3 then (alum, note) : student xs else student student xs  
+student (alum, note) : xs  = if note > 3 then (alum, note) : student xs else student student xs  
 
 --runhaskell class_ten.hs
 main = do 
