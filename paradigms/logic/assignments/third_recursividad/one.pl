d10f5fe13ea3858725f7b38ed8cbd5bc34b2e7d9
José Fernando Cordova <jfernancordova@gmail.com>
2019-10-15 22:43:41 -0300

    WIP

---
+++ paradigms/logic/assignments/third_recursividad/one.pl
@@ -0,0 +1,20 @@
+%swipl -s one.pl
+
+long([],0).
+long([_|T],L):- long(T,N), L is N+1.
+
+sum([],0).
+sum([X|F],S):- sum(F,O), S is O + X.
+
+promedio(M,P):- long(M,L), sum(M,S), P is S/L.
+
+maximo(X,Y,X) :- X >= Y.
+maximo(X,Y,Y) :- X < Y.
+max([X],X).
+max([X,Y|L],Z) :- max([Y|L],U), maximo(X,U,Z).
+
+minimo(X,Y,X) :- X =< Y.
+minimo(X,Y,Y) :- X > Y.
+
+min([X], X).
+min([X,Y|L], T) :- min([Y|L], P), minimo(X,P,T).
