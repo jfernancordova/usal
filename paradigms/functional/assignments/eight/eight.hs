cdb0b8c9598b01722fd23350ab0307007b117e22
José Fernando Cordova <jfernancordova@gmail.com>
2019-10-31 00:46:39 -0300

    WIP

--- paradigms/functional/assignments/eight/eight.hs
+++ paradigms/functional/assignments/eight/eight.hs
@@ -12,11 +12,15 @@ potCondImparList :: [Int] -> [Int]
 potCondImparList [] = []
 potCondImparList (x:xs) = if (mod x 2 /= 0) then potCond 2 x : potCondImparList xs else potCondImparList xs
 
+potGuardImparList :: [Int] -> [Int]
+potGuardImparList [] = []
+potGuardImparList (x:xs) = if (mod x 2 /= 0) then potGuard 2 x : potGuardImparList xs else potGuardImparList xs
 
 potComp :: [Integer]
 potComp = [2^n | n <- [10..50], mod n 2 /= 0]
 
 main = do
   print $ potComp
+  print $ potCondImparList [10..50] 
   print $ potCondImparList [10..50] 
 
