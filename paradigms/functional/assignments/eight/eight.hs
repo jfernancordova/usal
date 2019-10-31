3cabdb2c3f35893af01363a98532c65c48a37eec
José Fernando Cordova <jfernancordova@gmail.com>
2019-10-31 00:39:20 -0300

    WIP

--- paradigms/functional/assignments/eight/eight.hs
+++ paradigms/functional/assignments/eight/eight.hs
@@ -1,13 +1,22 @@
 --runhaskell eight.hs
 
-pot::Int -> Int -> Int
--- cut Conditions, remember recursivity
-pot _ 0 = 1
-pot base exp = base * pot base (exp - 1)
+potGuard :: Int->Int->Int
+potGuard base exp 
+  | exp==0  = 1   
+  | otherwise =  base * potGuard base (exp-1)
 
-potency :: Int -> [Int]
-potency n = takeWhile (<n) (map (2^) [0..])
+potCond :: Int -> Int -> Int
+potCond base exp = if exp==0 then 1 else base * potCond base (exp-1)
 
-main = do 
-  print $ pot 1 2
-  print $ potency 5
+potCondImparList :: [Int] -> [Int]
+potCondImparList [] = []
+potCondImparList (x:xs) = if (mod x 2 /= 0) then potCond 2 x : potCondImparList xs else potCondImparList xs
+
+
+potComp :: [Integer]
+potComp = [2^n | n <- [10..50], mod n 2 /= 0]
+
+main = do
+  print $ potComp
+  print $ potCondImparList [10..50] 
+
