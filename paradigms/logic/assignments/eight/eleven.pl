ff3c07fcbc86cdb140c8ea097f67500d75133b68
José Fernando Cordova <jfernancordova@gmail.com>
2019-11-01 15:57:08 -0300

    WIP

--- paradigms/logic/assignments/eight/eleven.pl
+++ paradigms/logic/assignments/eight/eleven.pl
@@ -1,7 +1,8 @@
 %swipl -s eleven.pl
 
-prod([],1).
-prod([N|L],X) :- par(N), prod(L,X1), X is X1 * N.
-prod([_N|L],X) :- not(par(_N)), prod(L,X).
-par(N):-N mod 2 =:= 0.
+par(X) :- 0 is mod(X, 2).
+impar(X) :- 1 is mod(X, 2).
 
+prod(_,[],[]):-!.
+prod(M, [X|XS], [Y|YS]):- par(X), prod(M, XS, YS), Y is (X*M).
+prod(M, [X|XS], [Y|YS]):- impar(X), prod(M, XS, YS), Y is X.
